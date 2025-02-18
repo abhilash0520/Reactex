7afc635a131d44270cd03a7d5ccd42c1675cec09
Abhilash Adepu <adepuabhilash88@gmail.com>
2025-02-18 10:27:55 +0530

    Componnents floder added

---
+++ src/assets/Components/Text1.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export class Text1 extends PureComponent {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Text1
